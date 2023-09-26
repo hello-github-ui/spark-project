6046f20cbc0925cb5cb710f758379421d7ca9937
hello-github-ui <util.you.come@gmail.com>
2023-09-26 13:17:56 +0800

    #spark-sql# Spark写入本地Parquet文件

---
+++ readme.md
@@ -0,0 +1,10 @@
+# 读写parquet
+## 报错
+### java.lang.UnsatisfiedLinkError: org.apache.hadoop.io.nativeio.NativeIO$Windows.access0(Ljava/lang/String;I)Z
+
+> 解决方案：
+>
+> https://blog.csdn.net/m0_53400772/article/details/130648598
+
+通过上述方案即可解决报错。
+
--- src/main/scala/com/example/spark/parquet/OperateFile.scala
+++ src/main/scala/com/example/spark/parquet/OperateFile.scala
@@ -1,4 +1,5 @@
 package com.example.spark.parquet
+
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -12,23 +13,17 @@ object OperateFile {
     def main(args: Array[String]): Unit = {
         val spark: SparkSession = SparkSession.builder()
                 .appName("Operate Parquet File")
+                // 避免生成 _SUCCESS 文件
                 .config("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+                // 避免生成  .crc 文件
+                .config("parquet.enable.summary-metadata", "false")
                 .master("local[*]")
                 .getOrCreate()
 
         spark.sparkContext.setLogLevel("ERROR")
 
         try {
-            // 获取当前项目的路径
-            //            var filePath: String = new File("").getCanonicalPath
-            //            filePath = filePath.replaceAll("\\\\", "/")
-            //            // println(s"""当前项目路径: ${filePath}""")
-            //
-            //            val df: DataFrame = spark.read.parquet("file:///" + filePath + "/datas/XHKG_Warrant_Snapshot_Level2_20230901.parquet")
-            //            df.show(10)
-
             saveParquet(spark)
-
         } finally {
             spark.stop()
         }
@@ -39,9 +34,35 @@ object OperateFile {
     写入数据到本地的parquet文件中
      */
     def saveParquet(spark: SparkSession): Unit = {
+        // 导入 SparkSession 的 隐式转换规则
+        import spark.implicits._
         // 模拟数据
-        val seq: Seq[(String, Any)] = Seq(("name", "zhangsan"), ("age", 21), ("address", "北京市昌平区大前门楼八字胡同2#13"))
-        val rdd: RDD[(String, Any)] = spark.sparkContext.parallelize(seq)
-        rdd.saveAsTextFile("datas/1.parquet")
+        val rdd: RDD[(Int, String, Int)] = spark.sparkContext.makeRDD(List((1, "zhangsan", 20), (2, "lisi", 30), (3, "wangwu", 19)))
+        // 这里的 toDF() 函数需要使用到 隐式转换规则，必须手动导入
+        val df: DataFrame = rdd.toDF("id", "username", "age")
+        df.show()
+
+        // 将 DataFrame 写入
+        // 获取当前项目的路径
+        var projectPath: String = new File("").getCanonicalPath
+        projectPath = projectPath.replaceAll("\\\\", "/")
+        // 输出的文件名路径
+        val filePath: String = "datas/user/"
+        df.write.parquet("file:///" + projectPath + "/" + filePath)
+
+    }
+
+    /*
+    fileStr: parquet文件的相对项目根路径的路径
+     */
+    def readParquet(spark: SparkSession, filePath: String): Unit = {
+        // 获取当前项目的路径
+        var projectPath: String = new File("").getCanonicalPath
+        projectPath = projectPath.replaceAll("\\\\", "/")
+        // println(s"""当前项目路径: ${filePath}""")
+
+        // val df: DataFrame = spark.read.parquet("file:///" + filePath + "/datas/XHKG_Warrant_Snapshot_Level2_20230901.parquet")
+        val df: DataFrame = spark.read.parquet("file:///" + projectPath + "/" + filePath)
+        df.show(10)
     }
 }
